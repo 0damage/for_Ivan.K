768f6d6ca17002e744e20fddb22abcc5c18c5e87
Rafael Shamilov <rafael_shamilov@mail.ru>
2016-05-24 03:27:55 +0300

    1

---
+++ entry_form.php
@@ -0,0 +1,24 @@
+<?php
+?><!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title>PHP</title>
+        <meta name="description" content="" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!--Подключение совместимости с edge -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <!-- адапт -->
+        <link rel="shortcut icon" href="favicon.png" /> <!-- фавиконка -->       
+</head>
+<body>
+    <form action="final.php" method="POST">
+        <input type="text" name="fieldsCount" placeholder="Введите fieldsCount">
+        <input type="text" name="chipCount" placeholder="Введите chipCount">
+        <input type ="submit" class="submit_login" name="submit" value="Войти">
+    </form>
+</body>
+</html>
---
+++ final.php
@@ -0,0 +1,87 @@
+﻿<?php
+if($_POST['submit']) {
+//Кол-во ячеек/Кол-во фишек
+$fieldsCount=$_POST['fieldsCount'];
+$chipCount=$_POST['chipCount'];
+//Ф-ция подсчета факториала числа
+function factorial($x) {
+    if($x===0) {
+        return 1;
+    }
+    else {
+        return factorial($x-1)*$x;
+    }
+}
+//Считаем факториал каждого входного числа
+$fieldsFactorial=factorial($fieldsCount);
+$chipFactorial=factorial($chipCount);
+//Ф-ция подсчета числа вариантов из кол-ва ячеек по кол-ву фишек.Получаем кол-во вариаций.
+function variants($fieldsCount,$chipCount,$fieldsFactorial,$chipFactorial) {
+    $u=$fieldsCount-$chipCount;
+    return $fieldsFactorial/(factorial($u)*$chipFactorial);
+}
+$v=variants($fieldsCount,$chipCount,$fieldsFactorial,$chipFactorial);
+    //Если вариаций больше 10 - "".Если вариаций меньше 10 - возвращает "Меньше 10 вариаций"
+    function less10($v) {
+        if($v>10) {
+            return "";
+        }
+        else {
+            return "Меньше 10 вариаций";
+        }
+    }
+    //Пустой прямугольник убирает при 
+    function rectangle($fieldsCount) {
+        if($fieldsCount>4) {
+            return "";
+        }
+        else {
+            return "style='display:none'";
+        }
+    }
+	//Не выводит число вариаций если меньше 10 вариаций 
+	function countfield($v) {
+        if($v<10) {
+            return "";
+        }
+        else {
+            return $v;
+        }
+    }
+	
+    //Выводит Ячейки
+    function qwe1($fieldsCount) {
+        if ($fieldsCount>4) {
+            for($i=1;$i<=$fieldsCount;$i++) {
+                echo "<td valign='top' align='center'>Ячейка {$i}</td>";
+            }
+        }
+    }
+    //Выводит Вариации
+    function qwe2($v) {
+        if ($v>10) {
+            for($i=1;$i<=$v;$i++) {
+                echo "<tr><td valign='top' align='center'>Вариант {$i}</td></tr>";
+            }
+        }
+    }
+}
+?>
+
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title></title>
+</head>
+<body>
+    <?php echo countfield($v); ?>
+    <?php echo less10($v); ?>
+<table border="1" cellpadding="7" cellspacing="0">
+   <tr>
+     <td valign='top' align='center' <?php echo rectangle($fieldsCount); ?>></td>
+     <?php echo qwe1($fieldsCount); ?>
+     <?php echo qwe2($v); ?> 
+   </tr>   
+  </table> 
+</body>
+</html>
